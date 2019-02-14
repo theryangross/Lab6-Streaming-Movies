beb4db1c502bb2ef1b91098786ba458222652449
Ryan Gross <ryangross123@icloud.com>
2019-02-14 12:15:43 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab_6_Streaming_Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new studioContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+
+            using (var db = new studioContext())
+            {
+                Studio CentryFox = new Studio
+                {  
+                    StudioName = "20th Century Fox",
+                    Movies = new List<Movie>
+                    {
+                        new Movie { Title = "Avatar", Genre = "Action"},
+                        new Movie { Title = "Deadpool", Genre = "Action"},
+                        new Movie { Title = "Apollo 13", Genre = "Drama"},
+                        new Movie { Title = "The Matrian", Genre = "Sci-Fi"}
+                    }
+                };
+
+                Studio UniversalPictures = new Studio
+                {
+                    StudioName = "Universal Pictures"
+                };
+            }
+
+            using (var db = new studioContext())
+            {
+                Movie Movies = new Movie { Title = "Jurassic Park", Genre = "Action"};
+                Studio studioToUpdate = db.Studio.Include(s => s.Movies).Where(s => s.)
+            }            
+
+            using (var db = new studioContext())
+            {
+
+            }
+        }
+    }
+}
---
+++ model.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Lab_6_Streaming_Movies
+{
+    public class studioContext : DbContext
+    {
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = database.db");
+        }
+        public DbSet<Studio> Studio {get; set;}
+        public DbSet<Movie> Movies {get; set;}
+    }
+
+    public class Studio
+    {
+        public string StudioID {get; set;}
+        public string StudioName {get; set;}
+        public List<Movie> Movies {get; set;} //Nav Prop
+
+        public override string ToString()
+        {
+            return $"Studio - {StudioID} : {StudioName}";
+        }
+    }
+
+    public class Movie
+    {
+        public string MovieID {get; set;}
+        public string Title {get; set;}
+        public string Genre {get; set;}
+        public string StudioID {get; set;} //FK
+        public Studio Studio {get; set;}
+
+         public override string ToString()
+        {
+            return $"Movie - {MovieID} : {Title} - {Genre}";
+        }
+    }
+}
